1e0f1460501764d45caebdd8dd009a7d133e3008
habiburrahman <habibur72345@gmail.com>
2023-01-03 18:45:15 +0600

    chart implement

--- js/main.js
+++ js/main.js
@@ -643,17 +643,23 @@ window.setting = function ()
 
                 }
 
+                let days = []
+
 window.dashboard= function () {
     return{
         getToken() {
             let match = document.cookie.match(new RegExp('(^| )token=([^;]+)'));
             if (match) return match[2];
         },
-
-
         dashBoardData:[],
         dashboardUserMessage:[],
         dashboardUserPerfomance:[],
+        dashboardChart1:[],
+        dashboardChart2:[],
+        chart1days:[],
+        chart1Data:[],
+        chart2days:[],
+        chart2Data:[],
 
         async getDashboard() {
             let  dashboard;
@@ -667,11 +673,195 @@ window.dashboard= function () {
             } else {
                 dashboard = [];
             }
-            // console.log(dashboard.data)
             this.dashBoardData = dashboard.data;
             this.dashboardUserMessage=this.dashBoardData.logs;
             this.dashboardUserPerfomance =this.dashBoardData.location_performance;
+            this.dashboardChart1=this.dashBoardData.location_active_day;
+            this.dashboardChart2=this.dashBoardData.location_active_month;
 
+            // console.log(this.dashboardChart)
+            for (const i in this.dashboardChart1) {
+                this.chart1days.push(i)
+                this.chart1Data.push(this.dashboardChart1[i])
+            }
+            for (const i in this.dashboardChart2) {
+                this.chart2days.push(i)
+                this.chart2Data.push(this.dashboardChart2[i])
+            }
+
+
+            // chart 01 starts from here
+            let ctx = document.getElementById("chart-bars").getContext("2d");
+            new Chart(ctx, {
+                type: "bar",
+                data: {
+                    labels: this.chart1days,
+                    datasets: [{
+                        label: "Sales",
+                        tension: 0.4,
+                        borderWidth: 0,
+                        borderRadius: 4,
+                        borderSkipped: false,
+                        backgroundColor: "rgba(255, 255, 255, .8)",
+                        data: this.chart1Data,
+                        maxBarThickness: 6
+                    },],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        }
+                    },
+                    interaction: {
+                        intersect: false,
+                        mode: 'index',
+                    },
+                    scales: {
+                        y: {
+                            grid: {
+                                drawBorder: false,
+                                display: true,
+                                drawOnChartArea: true,
+                                drawTicks: false,
+                                borderDash: [5, 5],
+                                color: 'rgba(255, 255, 255, .2)'
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 500,
+                                beginAtZero: true,
+                                padding: 10,
+                                font: {
+                                    size: 14,
+                                    weight: 300,
+                                    family: "Roboto",
+                                    style: 'normal',
+                                    lineHeight: 2
+                                },
+                                color: "#fff"
+                            },
+                        },
+                        x: {
+                            grid: {
+                                drawBorder: false,
+                                display: true,
+                                drawOnChartArea: true,
+                                drawTicks: false,
+                                borderDash: [5, 5],
+                                color: 'rgba(255, 255, 255, .2)'
+                            },
+                            ticks: {
+                                display: true,
+                                color: '#f8f9fa',
+                                padding: 10,
+                                font: {
+                                    size: 14,
+                                    weight: 300,
+                                    family: "Roboto",
+                                    style: 'normal',
+                                    lineHeight: 2
+                                },
+                            }
+                        },
+                    },
+                },
+            });
+            // chart 01 ends from here
+
+
+
+            // chart 02 starts from here
+            let ctx2 = document.getElementById("chart-line").getContext("2d");
+            new Chart(ctx2, {
+                type: "line",
+                data: {
+                    labels: this.chart2days,
+                    datasets: [{
+                        label: "Mobile apps",
+                        tension: 0,
+                        borderWidth: 0,
+                        pointRadius: 5,
+                        pointBackgroundColor: "rgba(255, 255, 255, .8)",
+                        pointBorderColor: "transparent",
+                        borderColor: "rgba(255, 255, 255, .8)",
+                        borderColor: "rgba(255, 255, 255, .8)",
+                        borderWidth: 4,
+                        backgroundColor: "transparent",
+                        fill: true,
+                        data: this.chart2Data,
+                        maxBarThickness: 6
+
+                    }],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        }
+                    },
+                    interaction: {
+                        intersect: false,
+                        mode: 'index',
+                    },
+                    scales: {
+                        y: {
+                            grid: {
+                                drawBorder: false,
+                                display: true,
+                                drawOnChartArea: true,
+                                drawTicks: false,
+                                borderDash: [5, 5],
+                                color: 'rgba(255, 255, 255, .2)'
+                            },
+                            ticks: {
+                                display: true,
+                                color: '#f8f9fa',
+                                padding: 10,
+                                font: {
+                                    size: 14,
+                                    weight: 300,
+                                    family: "Roboto",
+                                    style: 'normal',
+                                    lineHeight: 2
+                                },
+                            }
+                        },
+                        x: {
+                            grid: {
+                                drawBorder: false,
+                                display: false,
+                                drawOnChartArea: false,
+                                drawTicks: false,
+                                borderDash: [5, 5]
+                            },
+                            ticks: {
+                                display: true,
+                                color: '#f8f9fa',
+                                padding: 10,
+                                font: {
+                                    size: 14,
+                                    weight: 300,
+                                    family: "Roboto",
+                                    style: 'normal',
+                                    lineHeight: 2
+                                },
+                            }
+                        },
+                    },
+                },
+            });
+
+
+
+
+            // chart 02 ends from here
         },
     }
 }
+
+
