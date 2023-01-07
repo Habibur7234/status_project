dbc855f8d099f34311bd00b27836531fbf5dd520
habiburrahman <habibur72345@gmail.com>
2023-01-07 19:16:22 +0600

    last update

--- js/main.js
+++ js/main.js
@@ -1,101 +1,50 @@
-// window.dataTable = function () {
-// let data = [{"name":"Brielle Kuphal","email":"Brielle31@gmail.com","job":"Global Metrics Developer","country":"Tunisia","year":1969},{"name":"Barney Murray","email":"Barney75@gmail.com","job":"Product Solutions Executive","country":"Haiti","year":1970},{"name":"Ressie Ruecker","email":"Ressie.Ruecker30@gmail.com","job":"Forward Factors Orchestrator","country":"Georgia","year":1967},{"name":"Teresa Mertz","email":"Teresa_Mertz@hotmail.com","job":"Dynamic Division Director","country":"Bahrain","year":1953},{"name":"Chelsey Hackett","email":"Chelsey_Hackett@gmail.com","job":"Senior Accountability Architect","country":"Malta","year":1962},{"name":"Tatyana Metz","email":"Tatyana_Metz91@gmail.com","job":"Regional Metrics Coordinator","country":"Sierra Leone","year":1983},{"name":"Oleta Harvey","email":"Oleta_Harvey@yahoo.com","job":"District Usability Producer","country":"Swaziland","year":1963},{"name":"Bette Haag","email":"Bette.Haag99@gmail.com","job":"Dynamic Program Orchestrator","country":"Guyana","year":1974},{"name":"Meda Ebert","email":"Meda_Ebert8@yahoo.com","job":"Product Division Orchestrator","country":"Saint Kitts and Nevis","year":1954},{"name":"Elissa Stroman","email":"Elissa.Stroman6@yahoo.com","job":"Dynamic Applications Developer","country":"Andorra","year":1978},{"name":"Sid Swaniawski","email":"Sid_Swaniawski@hotmail.com","job":"Forward Interactions Designer","country":"Gibraltar","year":1979},{"name":"Madonna Hahn","email":"Madonna10@gmail.com","job":"Customer Configuration Specialist","country":"Pakistan","year":1948},{"name":"Waylon Kihn","email":"Waylon27@yahoo.com","job":"National Security Facilitator","country":"China","year":1958},{"name":"Jaunita Lindgren","email":"Jaunita_Lindgren@yahoo.com","job":"Senior Identity Consultant","country":"Sweden","year":1958},{"name":"Lenora MacGyver","email":"Lenora_MacGyver@yahoo.com","job":"Corporate Factors Manager","country":"Vietnam","year":1951},{"name":"Ole Collier","email":"Ole51@hotmail.com","job":"Corporate Factors Assistant","country":"Cape Verde","year":1967},{"name":"Taurean Koelpin","email":"Taurean.Koelpin@gmail.com","job":"District Intranet Officer","country":"Northern Mariana Islands","year":1980},{"name":"Thalia Yost","email":"Thalia73@gmail.com","job":"Dynamic Usability Director","country":"French Southern Territories","year":1948},{"name":"Okey Kling","email":"Okey84@gmail.com","job":"District Metrics Manager","country":"Poland","year":1953},{"name":"Darrick Ortiz","email":"Darrick73@yahoo.com","job":"Chief Metrics Agent","country":"Tajikistan","year":1990},{"name":"Eulalia Vandervort","email":"Eulalia.Vandervort63@gmail.com","job":"Future Creative Manager","country":"Spain","year":1990},{"name":"Genevieve Pouros","email":"Genevieve29@hotmail.com","job":"Investor Identity Facilitator","country":"British Indian Ocean Territory (Chagos Archipelago)","year":1983},{"name":"Horacio Purdy","email":"Horacio65@yahoo.com","job":"Direct Accountability Producer","country":"Slovakia (Slovak Republic)","year":1968},{"name":"Eleazar Konopelski","email":"Eleazar_Konopelski@hotmail.com","job":"National Usability Engineer","country":"Cayman Islands","year":1954},{"name":"Herminia Effertz","email":"Herminia.Effertz78@gmail.com","job":"Central Usability Manager","country":"Saudi Arabia","year":1954},{"name":"Brian Hermann","email":"Brian12@hotmail.com","job":"Central Optimization Coordinator","country":"Papua New Guinea","year":1952},{"name":"Wellington Barrows","email":"Wellington.Barrows@gmail.com","job":"Customer Identity Engineer","country":"Zimbabwe","year":1957},{"name":"David Rosenbaum","email":"David75@hotmail.com","job":"Global Research Administrator","country":"Mayotte","year":1958},{"name":"Pinkie Reilly","email":"Pinkie.Reilly67@gmail.com","job":"Internal Data Director","country":"Isle of Man","year":1973},{"name":"Asha Bartell","email":"Asha82@gmail.com","job":"Global Security Associate","country":"Tonga","year":1991},{"name":"Alexane Bode","email":"Alexane_Bode30@yahoo.com","job":"Product Integration Planner","country":"Honduras","year":1959},{"name":"Estelle Bode","email":"Estelle_Bode@yahoo.com","job":"Lead Marketing Producer","country":"Dominica","year":1962},{"name":"Jeromy Mayer","email":"Jeromy39@yahoo.com","job":"Direct Accountability Director","country":"Maldives","year":1973},{"name":"Ephraim Wiegand","email":"Ephraim88@gmail.com","job":"Direct Identity Administrator","country":"Australia","year":1946},{"name":"Jarrett Grimes","email":"Jarrett.Grimes99@gmail.com","job":"International Web Strategist","country":"Tunisia","year":1986},{"name":"Jeffry Macejkovic","email":"Jeffry_Macejkovic40@hotmail.com","job":"National Paradigm Specialist","country":"Taiwan","year":1959},{"name":"Janelle Stroman","email":"Janelle_Stroman@yahoo.com","job":"National Accounts Engineer","country":"Denmark","year":1974},{"name":"Camille Considine","email":"Camille79@gmail.com","job":"Central Solutions Planner","country":"Chile","year":1975},{"name":"Amani Schinner","email":"Amani.Schinner@yahoo.com","job":"Corporate Metrics Manager","country":"Sri Lanka","year":1981},{"name":"Dustin Bahringer","email":"Dustin76@hotmail.com","job":"Regional Research Designer","country":"Russian Federation","year":1988},{"name":"Mary Sanford","email":"Mary44@yahoo.com","job":"Chief Applications Consultant","country":"Isle of Man","year":1959},{"name":"Itzel Skiles","email":"Itzel32@gmail.com","job":"Dynamic Factors Strategist","country":"Western Sahara","year":1981},{"name":"Melyssa Denesik","email":"Melyssa_Denesik@hotmail.com","job":"Customer Security Consultant","country":"Libyan Arab Jamahiriya","year":1960},{"name":"Elenora McLaughlin","email":"Elenora_McLaughlin@yahoo.com","job":"Legacy Paradigm Engineer","country":"Swaziland","year":1954},{"name":"Chet Lueilwitz","email":"Chet_Lueilwitz98@yahoo.com","job":"Chief Implementation Producer","country":"Togo","year":1947},{"name":"Adrian Ondricka","email":"Adrian.Ondricka@hotmail.com","job":"National Optimization Orchestrator","country":"Monaco","year":1991},{"name":"Quincy West","email":"Quincy.West@yahoo.com","job":"Regional Paradigm Developer","country":"Qatar","year":1979},{"name":"Bernardo Krajcik","email":"Bernardo_Krajcik81@hotmail.com","job":"Lead Intranet Architect","country":"Japan","year":1959},{"name":"Aiyana Kshlerin","email":"Aiyana64@hotmail.com","job":"Forward Optimization Orchestrator","country":"Netherlands","year":1971},{"name":"Ambrose Anderson","email":"Ambrose32@yahoo.com","job":"Central Solutions Executive","country":"Moldova","year":1986},{"name":"Cassandre Volkman","email":"Cassandre.Volkman@yahoo.com","job":"Lead Quality Director","country":"Israel","year":1975},{"name":"Vivien Hettinger","email":"Vivien50@yahoo.com","job":"Investor Assurance Producer","country":"Suriname","year":1992},{"name":"Dejuan Auer","email":"Dejuan92@gmail.com","job":"Principal Functionality Architect","country":"Venezuela","year":1949},{"name":"Freddy Wunsch","email":"Freddy1@hotmail.com","job":"Direct Usability Designer","country":"Guinea-Bissau","year":1951},{"name":"Ole Will","email":"Ole_Will@gmail.com","job":"Future Operations Director","country":"Greece","year":1969},{"name":"Margie Kreiger","email":"Margie_Kreiger@gmail.com","job":"Corporate Paradigm Analyst","country":"India","year":1943},{"name":"Adrianna Heller","email":"Adrianna.Heller23@yahoo.com","job":"Future Directives Developer","country":"Democratic People's Republic of Korea","year":1962},{"name":"Helena Wuckert","email":"Helena.Wuckert34@hotmail.com","job":"Dynamic Metrics Executive","country":"Palestinian Territory","year":1962},{"name":"Boyd Bruen","email":"Boyd22@gmail.com","job":"Future Division Assistant","country":"United States Minor Outlying Islands","year":1947},{"name":"Herbert Gutkowski","email":"Herbert_Gutkowski@gmail.com","job":"Chief Mobility Specialist","country":"Lebanon","year":1954},{"name":"Jodie Jakubowski","email":"Jodie11@yahoo.com","job":"Forward Assurance Analyst","country":"India","year":1956},{"name":"Kathleen Mohr","email":"Kathleen.Mohr98@hotmail.com","job":"Senior Configuration Developer","country":"Singapore","year":1946},{"name":"Spencer Zieme","email":"Spencer.Zieme@yahoo.com","job":"Corporate Web Assistant","country":"Martinique","year":1957},{"name":"Marge Abshire","email":"Marge_Abshire40@hotmail.com","job":"Senior Applications Planner","country":"Albania","year":1982},{"name":"Kamron Muller","email":"Kamron3@yahoo.com","job":"Customer Response Strategist","country":"Dominican Republic","year":1969},{"name":"Betsy Wiegand","email":"Betsy.Wiegand@gmail.com","job":"Regional Implementation Assistant","country":"Finland","year":1985},{"name":"Tressa Hettinger","email":"Tressa.Hettinger@hotmail.com","job":"Lead Mobility Representative","country":"Montenegro","year":1986},{"name":"Bella Kunze","email":"Bella.Kunze@hotmail.com","job":"District Branding Designer","country":"Syrian Arab Republic","year":1975},{"name":"Bradford Quitzon","email":"Bradford3@yahoo.com","job":"Chief Solutions Facilitator","country":"Vanuatu","year":1992},{"name":"Adrien Gerhold","email":"Adrien73@yahoo.com","job":"Dynamic Infrastructure Supervisor","country":"Australia","year":1978},{"name":"Micheal Walter","email":"Micheal.Walter62@gmail.com","job":"Regional Communications Coordinator","country":"Saint Kitts and Nevis","year":1992},{"name":"Alberto Glover","email":"Alberto14@hotmail.com","job":"Product Web Liaison","country":"Saint Lucia","year":1962},{"name":"Quinn Stark","email":"Quinn.Stark46@hotmail.com","job":"District Markets Analyst","country":"Kyrgyz Republic","year":1977},{"name":"Carey Lemke","email":"Carey32@gmail.com","job":"Corporate Tactics Director","country":"Vanuatu","year":1944}];
-//
-//     return {
-//         items: [],
-//         view: 10,
-//         searchInput: '',
-//         pages: [],
-//         offset: 3,
-//         pagination: {
-//             total: data.length,
-//             lastPage: Math.ceil(data.length / 10),
-//             perPage: 10,
-//             currentPage: 1,
-//             from: 1,
-//             to: 1 * 10
-//         },
-//         currentPage: 1,
-//         async initData() {
-//             let response = await fetch('https://62b15c56196a9e987033e9c4.mockapi.io/api/1/status');
-//             if (response.ok) {
-//                 data = await response.json();
-//                 console.log(data);
-//             }
-//             this.items = data;
-//             this.showPages()
-//         },
-//
-//
-//         async initData() {
-//             fetch('http://location.local/api/user-list', {
-//                 method: 'GET',
-//                 headers: {'Authorization': 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYXRpb24ubG9jYWwvYXBpL2xvZ2luIiwiaWF0IjoxNjcwNDMyNzY1LCJleHAiOjE2NzA0MzYzNjUsIm5iZiI6MTY3MDQzMjc2NSwianRpIjoiUGFuVG1GTG82RHgzR053UyIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.uJh5yN7wlUffNARu55Scl9KCoFQfvrBtmBzUyjIGFTo'},
-//             })
-//                 .then((response) => {
-//                     if (!response.ok) {
-//                         data =response.json();
-//                         console.log(data);
-//                     }
-//                 })
-//
-//                 .catch((error) => {
-//                     console.log("error" + data)
-//                 })
-//         },
-//
-//
-//             checkView(index) {
-//             return index > this.pagination.to || index < this.pagination.from ? false : true
-//         },
-//
-//         changePage(page) {
-//             if (page >= 1 && page <= this.pagination.lastPage) {
-//                 this.currentPage = page
-//                 const total = this.items.length
-//                 const lastPage = Math.ceil(total / this.view) || 1
-//                 const from = (page - 1) * this.view + 1
-//                 let to = page * this.view
-//                 if (page === lastPage) {
-//                     to = total
-//                 }
-//                 this.pagination.total = total
-//                 this.pagination.lastPage = lastPage
-//                 this.pagination.perPage = this.view
-//                 this.pagination.currentPage = page
-//                 this.pagination.from = from
-//                 this.pagination.to = to
-//                 this.showPages()
-//             }
-//         },
-//         showPages() {
-//             const pages = []
-//             let from = this.pagination.currentPage - Math.ceil(this.offset / 2)
-//             if (from < 1) {
-//                 from = 1
-//             }
-//             let to = from + this.offset - 1
-//             if (to > this.pagination.lastPage) {
-//                 to = this.pagination.lastPage
-//             }
-//             while (from <= to) {
-//                 pages.push(from)
-//                 from++
-//             }
-//             this.pages = pages
-//         },
-//         changeView() {
-//             this.changePage(1)
-//             this.showPages()
-//         },
-//         isEmpty() {
-//             return this.pagination.total ? false : true
-//         }
-//     }
-// }
 
 
 const domain = 'https://location.selopian.us/api';
+
+
+function pushNotify() {
+    new Notify({
+        status: 'success',
+        title: 'Notify Title',
+        text: 'Notify text lorem ipsum',
+        effect: 'fade',
+        speed: 300,
+        customClass: null,
+        customIcon: null,
+        showIcon: true,
+        showCloseButton: true,
+        autoclose: false,
+        autotimeout: 3000,
+        gap: 20,
+        distance: 20,
+        type: 1,
+        position: 'right top'
+    })
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            //return c.substring(name.length, c.length);
+            return true;
+        }
+    }
+    return false;
+}
+
+if(!getCookie("token") && location.href.replace(/.*\/\/[^\/]*/, '') != "/status_project/login.html")
+{
+ window.location.replace("/status_project/login.html");
+}
+
+
 
 
 
@@ -199,6 +148,7 @@ window.services =
                 this.services = [];
 
                 this.services = userData.users.data;
+
                 this.firstPage = userData.users.from;
                 this.lastPage = userData.users.last_page;
                 this.totalPage = userData.users.last_page;
@@ -494,51 +444,64 @@ window.locationService =
                 } else {
                     locationData = [];
                 }
-                this.servicesLocation = locationData.locations.data;
+                this.servicesLocation = [];
 
-                this.lCurrentPage = locationData.locations.current_page;
+                this.servicesLocation = locationData.locations.data;
                 this.lFirstPage = locationData.locations.from;
                 this.lLastPage = locationData.locations.last_page;
-                this.lTotalPage = userData.users.last_page;
-
+                this.lTotalPage = locationData.locations.last_page;
+                this.lCurrentPage = locationData.locations.current_page;
 
 
 
 
                 let lPagination_number;
-                let lPegination_fatch = await fetch(domain + '/paginate-settings?name=users', {
+                let lPegination_fatch = await fetch(domain + '/paginate-settings?name=location', {
                     method: 'GET',
                     headers: {'Authorization': 'bearer ' + this.getToken()},
                 });
 
-
                 if (lPegination_fatch.ok) {
-                    lPagination_number = await pegination_fatch.json();
+                    lPagination_number = await lPegination_fatch.json();
                 } else {
                     lPagination_number = [];
                 }
-
                 this.lPagination = lPagination_number.per_page;
 
-
-
-                console.log(this.lPagination +"Habib");
-
-
-
-
-
-
-
-
-
-
-
-
-
+                console.log("Habib" + this.lPagination);
 
             },
 
+
+            lPaginationData:{
+                name:'location',
+                per_page:'',
+            },
+
+
+            updatelPagination() {
+                fetch(domain + '/paginate-settings-update', {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': 'bearer ' + this.getToken(),
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.lPaginationData),
+                })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error();
+                        }
+                        return response.json();
+                    })
+
+                    .then((data) => {
+
+                        this.initLocation();
+
+                    })
+
+            },
 
 
 
@@ -834,11 +797,7 @@ window.dashboard= function () {
                 this.chart1days.push(i)
                 this.chart1Data.push(this.dashboardChart1[i])
             }
-            //
-            // for (const i in this.dashboardChart1) {
-            //     this.chart1days.push(i)
-            //     this.chart1Data.push(this.dashboardChart1[i])
-            // }
+
             for (const i in this.dashboardChart2) {
                 this.chart2days.push(i)
                 this.chart2Data.push(this.dashboardChart2[i])
@@ -1186,7 +1145,9 @@ window.dashboard= function () {
             })
             // chart 04 ends from here
         },
-
+        // filterData(){
+        //     return this.dashboardUserPerfomance.filter(searchItem => searchItem.location_name.toLowerCase().includes(this.searchText.toLowerCase()))
+        // }
     }
 }
 
