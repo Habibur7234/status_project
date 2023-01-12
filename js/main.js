a988af5a1a08fded5521e7ae1c4fbe2668cd228b
habiburrahman <habibur72345@gmail.com>
2023-01-12 13:15:46 +0600

    clear user

--- js/main.js
+++ js/main.js
@@ -222,6 +222,7 @@ window.logOut =
 
                         window.location = "login.html"
                         document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                        document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
                         this.pushNotify('success', 'Success', 'Successfully LogOut ')
 
                     })
